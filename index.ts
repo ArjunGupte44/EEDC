41146ac01d1223f5a816b5d2a7e557f771ccdced
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-12-01 16:36:52 -0500

    fixed bash file

--- index.ts
+++ index.ts
@@ -5,9 +5,10 @@ const uri = 'mongodb://localhost:27017';
 const dbName = 'numbers';
 const collectionName = 'num';
 
-
 async function main(target: number) {
   const client = new MongoClient(uri);
+  
+  console.time('ExecutionTime'); // Start the timer
 
   try {
     // Connect to the MongoDB cluster
@@ -15,34 +16,36 @@ async function main(target: number) {
     const database = client.db(dbName);
     const collection = database.collection(collectionName);
 
-        // Find the closest number to the target number
-        // We sort by the absolute difference and get the first document
+    // Find the closest number to the target number
+    // We sort by the absolute difference and get the first document
 
-        let closest = null;
-        let smallestDiff = Number.MAX_SAFE_INTEGER;
+    let closest = null;
+    let smallestDiff = Number.MAX_SAFE_INTEGER;
 
-        // Cursor to iterate over all documents in the collection
-        const cursor = collection.find();
+    // Cursor to iterate over all documents in the collection
+    const cursor = collection.find();
 
-        // Loop through all documents to find the closest number
-        for await (const doc of cursor) {
-            const diff = Math.abs(target - doc.num);
-            if (diff < smallestDiff) {
-                smallestDiff = diff;
-                closest = doc;
-            }
-        };
-
-        if (closest) {
-            console.log(`The closest number to ${target} is ${closest.num} with _id: ${closest._id}`);
-        } else {
-            console.log('No numbers found in the database.');
+    // Loop through all documents to find the closest number
+    for await (const doc of cursor) {
+        const diff = Math.abs(target - doc.num);
+        if (diff < smallestDiff) {
+            smallestDiff = diff;
+            closest = doc;
         }
+    };
 
-    } finally {
-        // Close the connection to the MongoDB cluster
-        await client.close();
+    if (closest) {
+        console.log(`The closest number to ${target} is ${closest.num} with _id: ${closest._id}`);
+    } else {
+        console.log('No numbers found in the database.');
     }
-}
-main(390).catch(console.error);
 
+  } finally {
+      // Close the connection to the MongoDB cluster
+      await client.close();
+
+      console.timeEnd('ExecutionTime'); // End the timer and log the result
+  }
+}
+
+main(390).catch(console.error);
--- run_test.sh
+++ run_test.sh
@@ -6,22 +6,24 @@
 # Function to start powermetrics
 start_powermetrics() {
     # Start powermetrics with a sampling interval of 100ms (0.1 second)
-    sudo powermetrics -i 100 -f text -o "powermetrics_output.txt"
+    sudo powermetrics -i 100 -f text -o "powermetrics_output.txt" &
     # Save the PID of powermetrics
     POWERMETRICS_PID=$!
-    echo "Powermetrics started with PID: $POWERMETRICS_PID"
+    #echo "Powermetrics started with PID: $POWERMETRICS_PID"
 }
 
 # Function to stop powermetrics
 stop_powermetrics() {
     # Kill the powermetrics process
-    echo "Stopping powermetrics (PID: $POWERMETRICS_PID)..."
-    sudo pkill -TERM -P "$POWERMETRICS_PID"
+    #echo "Stopping powermetrics (PID: $POWERMETRICS_PID)..."
+    sleep 1
+    sudo kill $POWERMETRICS_PID
+
 }
 
 # Start powermetrics
 start_powermetrics
-
+sleep 1
 # Run your Node.js program
 node index.js
 
