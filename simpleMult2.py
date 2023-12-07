5fe4be5b26d14e040f85e8ee1b5d0c0bdfd17188
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-12-06 21:06:18 -0500

    python mult matrix

--- simpleMult2.py
+++ simpleMult2.py
@@ -1,26 +1,41 @@
+# Rewritten Code:
+# This function performs matrix multiplication.
+# It multiplies two matrices 'first' and 'second', storing the result in 'result'.
 def multiply_matrices(first, second, result, r1, c1, r2, c2):
+    # Iterate through rows of the first matrix
     for i in range(r1):
+        # Iterate through columns of the second matrix
         for j in range(c2):
-            # Initialize result[i][j] to 0 before summing
-            result[i][j] = 0
+            # Perform element-wise multiplication
+            prod = 1
             for k in range(c1):
-                result[i][j] += first[i][k] * second[k][j]
+                # Multiply current element from first and second matrices
+                prod *= first[i][k] * second[k][j]
+            # Accumulate the product in the result matrix
+            result[i][j] += prod
 
+# Main function to set up matrices and perform multiple multiplications
 def main(num_mult, mat_dim):
+    # Initialize the first matrix with a specific pattern
     first = [[i * j * 0.5 for j in range(mat_dim)] for i in range(mat_dim)]
+    # Initialize the second matrix with a different pattern
     second = [[(i + j) * 0.5 for j in range(mat_dim)] for i in range(mat_dim)]
-    result = [[None] for _ in range(mat_dim)] # All elements in result list replaced with None 
+    # Initialize the result matrix with zeros
+    result = [[0 for _ in range(mat_dim)] for _ in range(mat_dim)]
 
+    # Perform matrix multiplication 'num_mult' times
     for _ in range(num_mult):
-        # Initialize result with None for each list in range
-        for i in range(mat_dim):
-            result[i] = [None]*mat_dim
-            multiply_matrices(first, second, result, mat_dim, mat_dim, mat_dim, mat_dim)
+        multiply_matrices(first, second, result, mat_dim, mat_dim, mat_dim, mat_dim)
 
+    # Print a specific element of the result matrix as a sample output
     print("Result[5][5]:", result[5][5])
 
+# Entry point of the script
 if __name__ == "__main__":
     import sys
+    # Set the number of multiplications to perform
     num_mult = int(100)
-    mat_dim = int(1000)
+    # Set the dimension of the square matrices
+    mat_dim = int(100)
+    # Call the main function with these parameters
     main(num_mult, mat_dim)
