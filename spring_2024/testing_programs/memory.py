51a4e13cb4ec98ca864b09e2e1619737b69ae9a2
Leo Deng <leo7deng@gmail.com>
2024-10-11 18:06:02 -0400

    remove unecessary code

--- spring_2024/testing_programs/memory.py
+++
@@ -1,8 +0,0 @@
-def create_large_list(size):
-    large_list = []
-    for i in range(size):
-        large_list.append(" " * (1024 * 1024))  # Appending 1 MB of empty strings to the list
-    return large_list
-# Creating a list with 10,000 empty strings, each of size 1 MB
-my_large_list = create_large_list(10000)
-# Using a lot of memory for a relatively simple data structure
