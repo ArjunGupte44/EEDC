53c278a015d67ae03936e05597bac0abbe1f0032
Leo Deng <leo7deng@gmail.com>
2024-10-12 16:49:54 -0400

    bug fixes to compilation counter and pkl counter

--- energy/src/benchmark.py
+++ energy/src/benchmark.py
@@ -32,7 +32,7 @@ class Benchmark():
         print(f"Current directory: {current_dir}")
 
         #collect original data
-        if optim_iter: 
+        if optim_iter == 0: 
             try: 
                 subprocess.run(["make", "measure"], check=True)
                 print("Benchmark.run: make measure successfully\n")
@@ -56,7 +56,7 @@ class Benchmark():
         return results_file
 
 
-    def process_results(self, results_file, original, source_code_path) -> float:
+    def process_results(self, results_file, optim_iter, source_code_path) -> float:
         energy_data_file = open(f"{USER_PREFIX}/EEDC/energy/src/{self.benchmark_language}.csv", "r")
         benchmark_data = []
         for line in energy_data_file:
@@ -66,6 +66,7 @@ class Benchmark():
             
             #Remove empty strings for CPU, GPU, DRAM and convert remaining numbers to floats
             energy_data = [float(num) for num in energy_data if num]
+            print(energy_data)
             benchmark_data.append((benchmark_name, *energy_data))
 
         #Find average energy usage and average runtime
@@ -80,8 +81,7 @@ class Benchmark():
         #Append results to benchmark data dict
         source_code_file = open(source_code_path, "r")
         source_code = source_code_file.read()
-        if original:
-            self.benchmark_data[0] = (source_code, round(avg_energy, 3), round(avg_runtime, 3))
+        self.benchmark_data[optim_iter] = (source_code, round(avg_energy, 3), round(avg_runtime, 3))
 
         #Update PKL file with latest version of benchmark data dict
         with open(f"{USER_PREFIX}/EEDC/energy/{self.benchmark_language}/benchmark_data.pkl", "wb") as benchmark_data_pkl_file:
